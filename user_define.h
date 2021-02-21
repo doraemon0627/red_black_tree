d00b5cbd92fc639fc16da2f3a0df5a0f2dfd6a7e
doraemon0627 <69860154+doraemon0627@users.noreply.github.com>
2021-02-21 16:04:55 +0900

    Create user_define.h

---
+++ user_define.h
@@ -0,0 +1,13 @@
+#ifndef USER_DEFINE_H
+#define USER_DEFINE_H
+
+#define RED_BLACK_TREE
+/* #define AVL_TREE */
+
+typedef unsigned char bool;
+typedef bool RBTCOLOR;
+
+#define TRUE 1
+#define FALSE 0
+
+#endif
